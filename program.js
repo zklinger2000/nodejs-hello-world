c763e861ff5b6b2d7b19fbc6c02dce2a64f0d8d9
Zack <zklinger@gmail.com>
2015-05-19 13:27:47 -0700

    CHALLENGE 4: My First Asynch I/O

--- program.js
+++ program.js
@@ -1,8 +1,24 @@
+// CHALLENGE 4: My First Async I/O!
+var fs = require('fs');
+//the 1st two args returned are always 'node' then the home path so start 
+//at argv[2]
+var file = process.argv[2];
+
+//readFile() is the asynchronous Node way to read file buffers
+fs.readFile(file, function (err, contents) {
+  // fs.readFile(file, 'utf8', callback) can also be used
+  var lines = contents.toString().split('\n').length - 1;
+  console.log(lines);
+});
+
+/*
 // CHALLENGE 3: Reads from a file and counts the newlines in it
 
 //include the fs (filesystem) library into a variable named fs
 var fs = require('fs');
 
+//the 1st two args returned are always 'node' then the home path so start 
+//at argv[2]
 var contents = fs.readFileSync(process.argv[2]);
 var lines = contents.toString().split('\n').length - 1;
 console.log(lines);
@@ -11,6 +27,7 @@ console.log(lines);
 // second argument to readFileSync, then you'll get a String!
 //
 // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+*/
 
 
 /*// CHALLENGE 2: Passing arguments from the console through process.argv
